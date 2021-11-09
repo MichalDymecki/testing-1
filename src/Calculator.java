8e32a84403c9c1c2f319baa60e6812f459996d2e
Michal Dymecki <mdymecki76@gmail.com>
2021-11-09 21:03:51 +0100

    add out to gitignore

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj pierwszą liczbę: ");
+        int firstNumber = scanner.nextInt();
+        System.out.print("Podaj drugą liczbę: ");
+        int secondNumber = scanner.nextInt();
+        double thirdNumber = secondNumber;
+
+        int adding = firstNumber + secondNumber;
+        int subtraction = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        double division = firstNumber / thirdNumber;
+        int mod = secondNumber % firstNumber;
+
+        System.out.println("Wynik dodawania to : " + adding);
+        System.out.println("Wynik odejmowania to : " + subtraction);
+        System.out.println("Wynik mnożenia to : " + multiplication);
+        System.out.println("Wynik dzielenia to : " + division);
+        System.out.println("Wynik modulo to : " + mod);
+    }
 }
