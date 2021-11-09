2f9a02d950b1ad91bb356d6c10ecb81ac7d85938
Michał-laptopDell <mdymecki87@gmail.com>
2021-11-09 22:07:48 +0100

    instrukcje warunkowe

---
+++ src/AgeChecker.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AgeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your age?");
+
+        int customerAge = scanner.nextInt();
+
+        if (customerAge >= 18){
+            System.out.println("Wanna buy some alcohol? No problem, thanks");
+        }else{
+            System.out.println("You can't buy alcohol until you reach age of 18");
+        }
+
+    }
+}
